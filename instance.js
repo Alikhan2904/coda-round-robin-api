382c03e2f2f15b12c4808c61acad6644198849df
Alikhan2904 <alikhan.sachwani@gmail.com>
2024-09-17 23:37:39 +0500

    added instance file and remove server

--- instance.js
+++ instance.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 app.use(express.json());
 
-app.post("/api", (req, res) => {
+app.post('/api', (req, res) => {
   res.status(200).json(req.body);
 });
 
