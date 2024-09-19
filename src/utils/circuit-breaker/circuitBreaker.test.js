c522378ccb01d962fddbaeba4b100be3836371aa
Alikhan2904 <alikhan.sachwani@gmail.com>
2024-09-20 03:39:38 +0500

    refactor circuitBreaker tests

--- src/utils/circuit-breaker/circuitBreaker.test.js
+++ src/utils/circuit-breaker/circuitBreaker.test.js
@@ -5,48 +5,49 @@ import {
 } from './circuitBreaker';
 
 describe('Circuit Breaker', () => {
-  beforeEach(() => {
+  const instance = 'http://localhost:3001/api';
+  jest.useFakeTimers();
+
+  afterAll(() => {
     jest.clearAllTimers();
   });
 
   it('should not trip the circuit initially', () => {
-    expect(isCircuitTripped('instance1')).toBe(false);
+    expect(isCircuitTripped(instance)).toBe(false);
   });
 
   it('should register failures correctly', () => {
-    registerFailure('instance1');
-    expect(isCircuitTripped('instance1')).toBe(false);
+    registerFailure(instance);
+    expect(isCircuitTripped(instance)).toBe(false);
 
-    registerFailure('instance1');
-    expect(isCircuitTripped('instance1')).toBe(false);
+    registerFailure(instance);
+    expect(isCircuitTripped(instance)).toBe(false);
 
-    registerFailure('instance1');
-    expect(isCircuitTripped('instance1')).toBe(true);
+    registerFailure(instance);
+    expect(isCircuitTripped(instance)).toBe(true);
   });
 
   it('should reset the circuit on success', () => {
-    registerFailure('instance1');
-    registerFailure('instance1');
-    registerFailure('instance1');
+    registerFailure(instance);
+    registerFailure(instance);
+    registerFailure(instance);
 
-    expect(isCircuitTripped('instance1')).toBe(true);
+    expect(isCircuitTripped(instance)).toBe(true);
 
-    registerSuccess('instance1');
+    registerSuccess(instance);
 
-    expect(isCircuitTripped('instance1')).toBe(false);
+    expect(isCircuitTripped(instance)).toBe(false);
   });
 
   it('should reset after timeout', () => {
-    jest.useFakeTimers();
+    registerFailure(instance);
+    registerFailure(instance);
+    registerFailure(instance);
 
-    registerFailure('instance1');
-    registerFailure('instance1');
-    registerFailure('instance1');
-
-    expect(isCircuitTripped('instance1')).toBe(true);
+    expect(isCircuitTripped(instance)).toBe(true);
 
     jest.advanceTimersByTime(30000);
 
-    expect(isCircuitTripped('instance1')).toBe(false);
+    expect(isCircuitTripped(instance)).toBe(false);
   });
 });
